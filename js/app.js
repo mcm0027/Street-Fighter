b6cac128d3cbbc9c92b7c1257234cc08a112bfad
Matt Morris <mcm0027@yahoo.com>
2015-10-04 19:36:25 -0500

    add hadouken sound

--- js/app.js
+++ js/app.js
@@ -1,8 +1,53 @@
+function playHadouken () {
+  $('#hadouken')[0].volume = 0.5;
+  $('#hadouken')[0].load();
+  $('#hadouken')[0].play();
+}
 
-$(".ryu-ready").hide();
-$("img").hide();
+$(document).ready(function() {
 
-$("div.ryu-img")
-	.mouseenter(function() {
-		$("div.ryu-still").hide();
+	$(".ryu-ready").hide();
+	$(".ryu-throwing").hide();
+	$(".hadouken").hide();
+	$(".ryu-cool").hide();
+
+	$("div.ryu-img")
+		.mouseenter(function() {
+			$("div.ryu-still").hide();
+			$(".ryu-ready").show();
+		})
+	.mouseleave(function() {
+			$(".ryu-ready").hide();
+			$(".ryu-throwing").hide();
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		});
+
+	$("div.ryu-img")
+		.mousedown(function() {
+			playHadouken();
+			$("div.ryu-still").hide();
+			$(".ryu-cool").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-throwing").show();
+			$(".hadouken").finish().show()
+				.animate({ "left": "+=520px" }, "slow", 
+					function() {
+						$(this).hide();
+						$(this).css("left", "520px")
+				});
+		})
+		.mouseup(function() {
+			$(".ryu-throwing").hide();
+			$(".ryu-cool").hide();
+			$(".ryu-ready").show();
+		});
+	$("div.ryu-img").keydown(function(e){
+		if(e.keyCode == 88){
+			$(".ryu-cool").show();
+			}
 	});
+
+});
+
+
